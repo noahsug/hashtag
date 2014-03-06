003333df4e68344b55fca42d80d1f3d5f8759ef2
sugarman <sugarman@google.com>
2014-03-06 11:13:22 -0800

    quick fix

--- code.js
+++ code.js
@@ -1,10 +1,12 @@
 window.addEventListener(function(e) {
+  console.log('w', e.which, 's', e.shiftKey, '#?', e.which == '51' && e.shiftKey);
   if (e.which == '51' && e.shiftKey) {
     maybeHashtagify();
   }
 });
 
 var maybeHashtagify = function() {
+  console.log('INPUT:', document.activeElement.value);
   var inputText = document.activeElement.value;
   if (inputText) {
     var outputText = '#' + inputText;
