f2778114f0cc36cb7e69c73085b33be273dad8eb
sugarman <sugarman@google.com>
2014-03-06 11:09:40 -0800

    quick fix

--- code.js
+++ code.js
@@ -1,1 +1,13 @@
-document.activeElement.value = 'noah sugarman';
+window.addEventListener(function(e) {
+  if (e.which == '51' && e.shiftKey) {
+    maybeHashtagify();
+  }
+});
+
+var maybeHashtagify = function() {
+  var inputText = document.activeElement.value;
+  if (inputText) {
+    var outputText = '#' + inputText;
+    document.activeElement.value = outputText;
+  }
+}
